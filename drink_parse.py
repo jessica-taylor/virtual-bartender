5106e7d614e5a4d3dbf19cbe880568c14670b5f1
Mario John Villaplana <mario.villaplana@gmail.com>
2014-05-10 15:11:22 -0700

    we now get drink titles also

--- drink_parse.py
+++ drink_parse.py
@@ -18,19 +18,40 @@ CATEGORY_PAGES = ['http://www.drinksmixer.com/cat/1/',
     'http://www.drinksmixer.com/cat/22/']
 
 def main():
-  url_suffixes = find_drink_url_suffixes()
-  urls = [DOMAIN + suffix for suffix in url_suffixes]
+  cat_url_suffixes = find_drink_url_suffixes_and_cat()
+  print(len(cat_url_suffixes))
 
-def find_drink_url_suffixes():
+  drinks = set()
+
+  for (category, suffix) in cat_url_suffixes:
+    drink = {}
+    drink['category'] = category
+
+    f = urllib2.urlopen(DOMAIN + suffix)   
+    html = f.read()
+
+    drink['title'] = get_drink_title(html)
+
+# Returns a list of tuples whose first element is the drink category and whose
+# second element is a URL pointing to a particular drink in that category.
+def find_drink_url_suffixes_and_cat():
   suffixes = []
   for url in CATEGORY_PAGES:
     f = urllib2.urlopen(url)
     html = f.read()
     soup = BeautifulSoup(html)
+    category = soup.find('div', {'class': 'pm'}).find('h1').text
+    category = category.replace(' recipes', '')
     results = soup.find('p', {'class': 'l2c'}).findAll('a')
     for result in results: 
-      suffixes.append(result['href'])
+      suffixes.append((category, result['href']))
   return suffixes
+
+def get_drink_title(html):
+  soup = BeautifulSoup(html)
+  title = soup.find('h1', {'class': 'recipe_title'}).text.replace(' recipe',
+      '')
+  return title
 
 if __name__ == "__main__":
   main()
